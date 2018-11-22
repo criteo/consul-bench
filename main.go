b965184af71bb8d4799d298a5846105a05706c82
Thibault Gilles <t.gilles@criteo.com>
2018-11-22 16:40:29 +0100

    Cleanup

--- main.go
+++ main.go
@@ -1,119 +1,110 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
-	"sync"
-	"sync/atomic"
 	"time"
 
 	consul "github.com/hashicorp/consul/api"
 )
 
-const (
-	InstanceCount = 800
-	Watchers      = 150
-	FlapInterval  = 800 * time.Second
-)
-
-type IdxStat struct {
-	Acks      int
-	ChangedAt time.Time
-	FirstAck  time.Time
-	LastAck   time.Time
-}
-
 func main() {
-	c, err := consul.NewClient(&consul.Config{})
+	consulAddr := flag.String("consul", "127.0.0.1:8500", "Consul address")
+	serviceName := flag.String("service", "srv", "Service to watch")
+	registerInstances := flag.Int("register", 0, "Register N -service instances")
+	flapInterval := flag.Duration("flap-interval", 0, "If -register is given, flap each instance between critical and passing state on given interval")
+	watchers := flag.Int("watchers", 1, "Number of concurrnet watchers on service")
+	flag.Parse()
+
+	c, err := consul.NewClient(&consul.Config{
+		Address: *consulAddr,
+	})
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	idx := uint64(0)
-	stats := map[uint64]IdxStat{}
-	statsLock := sync.Mutex{}
+	startCh := make(chan struct{})
 
-	for instanceID := 0; instanceID < InstanceCount; instanceID++ {
-		err := c.Agent().ServiceRegister(&consul.AgentServiceRegistration{
-			Name: "srv",
-			ID:   fmt.Sprintf("srv-%d", instanceID),
-			Checks: []*consul.AgentServiceCheck{
-				{
-					CheckID: fmt.Sprintf("check-%d", instanceID),
-					TTL:     "1m",
+	if *registerInstances > 0 {
+		log.Printf("Registering %d %s instances...\n", *registerInstances, *serviceName)
+
+		for instanceID := 0; instanceID < *registerInstances; instanceID++ {
+			err := c.Agent().ServiceRegister(&consul.AgentServiceRegistration{
+				Name: "srv",
+				ID:   fmt.Sprintf("srv-%d", instanceID),
+				Checks: []*consul.AgentServiceCheck{
+					{
+						CheckID: fmt.Sprintf("check-%d", instanceID),
+						TTL:     "1m",
+						Status:  consul.HealthCritical,
+					},
 				},
-			},
-		})
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-
-	for instanceID := 0; instanceID < InstanceCount; instanceID++ {
-		go func(instanceID int) {
-			time.Sleep(FlapInterval / time.Duration(InstanceCount) * time.Duration(instanceID))
-			lastStatus := false
-			for {
-				nidx := atomic.AddUint64(&idx, 1)
-				var f func(checkID, note string) error
-				if lastStatus {
-					f = c.Agent().FailTTL
-				} else {
-					f = c.Agent().PassTTL
-				}
-
-				err := f(fmt.Sprintf("check-%d", instanceID), fmt.Sprint(nidx))
-				if err != nil {
-					log.Fatal(err)
-				}
-
-				statsLock.Lock()
-				stats[nidx] = IdxStat{
-					ChangedAt: time.Now(),
-				}
-				statsLock.Unlock()
-
-				lastStatus = !lastStatus
-
-				time.Sleep(FlapInterval)
+			})
+			if err != nil {
+				log.Fatal(err)
 			}
-		}(instanceID)
+		}
+
+		if *flapInterval > 0 {
+			log.Printf("Flapping instances every %s", *flapInterval)
+
+			for instanceID := 0; instanceID < *registerInstances; instanceID++ {
+				go func(instanceID int) {
+					<-startCh
+
+					time.Sleep((*flapInterval / time.Duration(*registerInstances)) * time.Duration(instanceID))
+					lastStatus := false
+					for {
+						var f func(checkID, note string) error
+						if lastStatus {
+							f = c.Agent().FailTTL
+							fmt.Print("F")
+						} else {
+							f = c.Agent().PassTTL
+							fmt.Print("P")
+						}
+
+						err := f(fmt.Sprintf("check-%d", instanceID), "")
+						if err != nil {
+							log.Fatal(err)
+						}
+						lastStatus = !lastStatus
+
+						time.Sleep(*flapInterval)
+					}
+				}(instanceID)
+			}
+		}
+
+		log.Println("Services registered")
 	}
 
-	log.Println("services registered")
+	log.Println("Starting", *watchers, "watchers on", *serviceName, "...")
 
-	for watcherID := 0; watcherID < Watchers; watcherID++ {
+	for watcherID := 0; watcherID < *watchers; watcherID++ {
 		go func() {
 			lastIdx := uint64(0)
 
 			for {
-				entries, meta, err := c.Health().Service("srv", "", false, &consul.QueryOptions{
-					WaitTime:  10 * time.Minute,
-					WaitIndex: lastIdx,
+				_, meta, err := c.Health().Service(*serviceName, "", false, &consul.QueryOptions{
+					WaitTime:   10 * time.Minute,
+					WaitIndex:  lastIdx,
+					AllowStale: true,
 				})
 				if err != nil {
 					log.Fatal(err)
 				}
 
 				lastIdx = meta.LastIndex
-
-				//maxIdx := uint64(0)
-				for _, entry := range entries {
-					for _, c := range entry.Checks {
-						if c.ServiceID == "" {
-							continue
-						}
-
-						if c.Notes == "" {
-							continue
-						}
-
-						fmt.Println("Check: ", c.Notes)
-					}
-				}
+				fmt.Print(".")
 			}
 		}()
 	}
+
+	log.Println("Watchers started.")
+
+	close(startCh)
 
 	<-make(chan bool)
 }
